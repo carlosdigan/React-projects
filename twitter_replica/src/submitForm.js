7215ebf72cb1c339a61b4e12e314eedb8abecb69
mrcarlosdigan <93342111+mrcarlosdigan@users.noreply.github.com>
2021-11-20 23:18:50 +0000

    Add files via upload

---
+++ twitter_replica/src/submitForm.js
@@ -0,0 +1,15 @@
+import { v4 } from "uuid"
+export default function submitForm(el, content, tweets, setTweets) {
+    const newTweet =  {
+        author: {name: el.target.author.value , handle: "@" + el.target.handle.value}, 
+        content: {text: content},
+        id: v4(),
+        comments: 0,
+        likes: 0,
+        retweets: 0,
+        commented: false,
+        liked: false,
+        retweeted: false
+    }
+    setTweets([newTweet, ...tweets])
+}
